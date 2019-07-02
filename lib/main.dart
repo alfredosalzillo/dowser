d8fb1ad5721709c17809020be7fa5099a6549d1c
alfredo.salzillo <noctisbels@gmai.com>
2019-07-02 23:32:16 +0200

    add map implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flhooks/flhooks.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -26,53 +31,100 @@ Position usePosition(
   return useStream(() => geolocator.getPositionStream(locationOptions), []);
 }
 
+class AppConfig extends InheritedWidget {
+  final double defaultZoom;
+
+  AppConfig({
+    this.defaultZoom = 15.0,
+    key,
+    child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return false;
+  }
+
+  static AppConfig of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(AppConfig);
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dowser',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Dowser'),
+      home: AppConfig(
+        defaultZoom: 15.0,
+        child: HomePage(title: 'Dowser'),
+      ),
     );
   }
 }
 
-class MyHomePage extends HookWidget {
-  MyHomePage({this.title}) : super();
+class HomePage extends HookWidget {
+  HomePage({this.title}) : super();
 
   final String title;
 
   @override
   Widget builder(BuildContext context) {
-    final counter = useState(0);
+    final AppConfig config = AppConfig.of(context);
     final position = usePosition();
+    final _controller = useMemo(() => Completer<GoogleMapController>(), []);
+    final cameraController = useAsync(() => _controller.future, []);
+    final zoom = useState(config.defaultZoom);
+    useEffect(() {
+      if (cameraController != null && position != null) {
+        cameraController.animateCamera(CameraUpdate.newLatLngZoom(
+            LatLng(
+              position.latitude,
+              position.longitude,
+            ),
+            zoom.value));
+      }
+    }, [position?.longitude, position?.latitude, cameraController]);
+    final markers = useStream<List>(
+        () => position == null
+            ? Stream.empty()
+            : Geolocator()
+                .getPositionStream(
+                  LocationOptions(
+                    accuracy: LocationAccuracy.high,
+                    distanceFilter: 0,
+                  ),
+                )
+                .asyncMap((position) => http.get(
+                    'https://untitled-7n0vxwvqdc4j.runkit.sh/?lng=${position.longitude}&lat=${position.latitude}'))
+                .map((response) => json.decode(response.body) as List),
+        [position?.longitude, position?.latitude]);
     return Scaffold(
       appBar: AppBar(
         title: Text(this.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Current position ${position?.latitude}, ${position?.longitude}',
-            ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '${counter.value}',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => counter.value += 1,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(target: LatLng(0, 0)),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        onCameraMove: (cameraPosition) {
+          zoom.value = cameraPosition.zoom;
+        },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        markers: [
+          if (markers != null)
+            ...markers.map((json) => Marker(
+                  markerId: MarkerId(json['id']),
+                  position: LatLng(
+                      double.parse(json['lat']), double.parse(json['lng'])),
+                )),
+        ].toSet(),
       ),
     );
   }
